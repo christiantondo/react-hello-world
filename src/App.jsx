21953124946a1e6a87cfc1d658972cd84199daea
christiantondo <christiantondo1@gmail.com>
2025-12-03 17:55:36 +0100

    react function rename and class fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function HelloReact() {
   return (
-    <>
-      <div>
-        <h1 class="logo">Hello React World</h1>
-        <h2 >Nice to meet you<span class="heart">♥</span></h2>
-      </div>
-    </>
+    <div>
+      <img src="/src/assets/react.svg" className="logo" alt="React Logo" />
+      <h1>Hello React World</h1>
+      <h2 >Nice to meet you<span className="heart">♥</span></h2>
+    </div>
   )
 }
 
-export default App
+export default HelloReact
