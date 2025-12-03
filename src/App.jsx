2600aa611046580c193994e4a4663d49fb081ac4
christiantondo <christiantondo1@gmail.com>
2025-12-03 02:39:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 class="logo">Hello React World</h1>
+        <h2 >Nice to meet you<span class="heart">♥</span></h2>
+      </div>
+    </>
+  )
+}
+
+export default App
